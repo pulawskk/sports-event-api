7513162dc19b5726a779b8cd2170d231156ed98b
KarolPulawski <karol.j.pulawski@gmail.com>
2019-09-27 21:48:22 +0200

    Initial commit

---
+++ src/test/java/com/pulawskk/sportseventapi/SportsEventApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pulawskk.sportseventapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportsEventApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
