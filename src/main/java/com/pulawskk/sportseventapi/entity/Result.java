15ada6a38759447911751921ce3543939393e378
KarolPulawski <karol.j.pulawski@gmail.com>
2019-09-28 14:55:35 +0200

    Create Result entity. Closes #9.

---
+++ src/main/java/com/pulawskk/sportseventapi/entity/Result.java
@@ -0,0 +1,25 @@
+package com.pulawskk.sportseventapi.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+
+@Entity
+@Table(name="results")
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "game_id", referencedColumnName = "id")
+    private Game game;
+
+}
