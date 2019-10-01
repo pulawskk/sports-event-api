b6752b523f6b3f02cba4bb7b8b30365798b49205
KarolPulawski <karol.j.pulawski@gmail.com>
2019-10-01 22:47:46 +0200

    Add TDD class for FakeFootballService. #43.

--- src/test/java/com/pulawskk/sportseventapi/service/impl/FakeFootballServiceTest.java
+++ src/test/java/com/pulawskk/sportseventapi/service/impl/FakeFootballServiceTest.java
@@ -21,18 +21,32 @@ class FakeFootballServiceTest {
 
         //helper method to generate pair values from 1 to number of teams
 
-        //return set of games, game has only start date, end date, teamHome, teamAway
+        //return set of games, game has only start date, end date, teamHome, teamAway, competition
 
-        //game should have status: BEFORE_ODDS / ODDS / READY_FOR_RESULT
+        //game should have status: BEFORE_ODDS / ODDS / RESULTED
     }
 
     @Test
     void generateOdds() {
+        //pass games with teams -> check if their status is BEFORE_ODDS
 
+        //randomly or based on history/form of each team create odds and generate value for them
+
+        //game has teamHome, teamAway, starDate, endDate, competition, odds
+
+        //game status is ODDS
     }
 
     @Test
     void generateResult() {
+        //pass games with odds -> check if their status is ODDS
 
+        //based on form/history or randomly generate results for each game -> create game report
+
+        //create Result with game and created game report
+
+        //game can change status for RESULTED
+
+        //return set of results
     }
 }
