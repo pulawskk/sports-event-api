2f9fb8b2b5411fd711be1da18722633948fcb21f
KarolPulawski <karol.j.pulawski@gmail.com>
2019-11-17 22:12:37 +0100

    Cut string to keep 2 decimal numbers. Close #84.

--- src/main/java/com/pulawskk/sportseventapi/AppConfig.java
+++ src/main/java/com/pulawskk/sportseventapi/AppConfig.java
@@ -1,9 +1,5 @@
 package com.pulawskk.sportseventapi;
 
-import com.pulawskk.sportseventapi.service.impl.HttpPostServiceImpl;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
