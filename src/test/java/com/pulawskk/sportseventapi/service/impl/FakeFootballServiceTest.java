f4d654d7172638f78788cb2a2b99ab3ef2196350
Małgorzata Dubel <goska.dubel@gmail.com>
2020-05-31 16:28:49 +0200

    #93 Unit tests to cover FakeFootballService.

--- src/test/java/com/pulawskk/sportseventapi/service/impl/FakeFootballServiceTest.java
+++ src/test/java/com/pulawskk/sportseventapi/service/impl/FakeFootballServiceTest.java
@@ -1,6 +1,7 @@
 package com.pulawskk.sportseventapi.service.impl;
 
 import com.pulawskk.sportseventapi.entity.*;
+import com.pulawskk.sportseventapi.enums.CompetitionType;
 import com.pulawskk.sportseventapi.enums.GameOddType;
 import com.pulawskk.sportseventapi.enums.GameStatus;
 import com.pulawskk.sportseventapi.service.*;
@@ -17,8 +18,8 @@ import java.util.stream.Stream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 class FakeFootballServiceTest {
 
@@ -51,6 +52,9 @@ class FakeFootballServiceTest {
     private Competition competition;
     private Set<Team> teams;
 
+    private Team chelseaTeam;
+    private Team arsenalTeam;
+
 
     @BeforeEach
     void setUp() {
@@ -69,10 +73,16 @@ class FakeFootballServiceTest {
                                     "Everton", "Liverpool",
                                     "Watford", "Wolverhampton Wanders",
                                     "Bournemouth", "Burnley")
-                                .map((teamName) -> new Team(teamName))
+                                .map(Team::new)
                                 .collect(Collectors.toSet());
         competition.setTeams(teams);
         teams.forEach(team -> team.setCompetitions(competitions));
+
+        chelseaTeam = Team.builder().id(31L).name("Chelsea")
+                .competitions(competitions).build();
+
+        arsenalTeam = Team.builder().id(32L).name("Arsenal")
+                .competitions(competitions).build();
     }
 
     @Test
@@ -119,8 +129,11 @@ class FakeFootballServiceTest {
     }
 
     @Test
-    void shouldReturnSetOfResult_whenSetOfGamesWithOddsIsGiven() {
+    void shouldReturnSetOfResult_whenSetOfGamesWithOddsIsGivenAndCompetitionIsLeague() {
+        competition.setType(CompetitionType.LEAGUE);
         when(teamService.findAllByCompetitions(competition)).thenReturn(teams);
+
+//        Competition competitionLeague = Competition.builder().id(4L).type(CompetitionType.LEAGUE).build();
         Game gameForSave = Game.builder().id(1L).build();
         Set<Game> gamesForSave = new HashSet<>();
         gamesForSave.add(gameForSave);
@@ -148,6 +161,59 @@ class FakeFootballServiceTest {
             assertThat(result.iterator().next().getGameReport().getGoalHome(), greaterThan(-1));
             assertThat(result.iterator().next().getGameReport().getCornerAway(), greaterThan(-1));
         });
+
+        verify(resultFootballService, times(1)).saveAll(anySet());
+        verify(competitionService, never()).save(any());
+        verify(teamService, never()).save(any());
+        verify(teamService, never()).findByName(anyString());
+    }
+
+    @Test
+    void shouldReturnSetOfResult_whenSetOfGamesWithOddsIsGivenAndCompetitionIsTournamentRounds() {
+        Competition competitionTournamentRounds = Competition.builder().id(4L).type(CompetitionType.TOURNAMENT_ROUNDS).name("FA Cup").build();
+        Set<Team> teams = new HashSet<>();
+        teams.add(chelseaTeam);
+        teams.add(arsenalTeam);
+        competitionTournamentRounds.setTeams(teams);
+        Set<Competition> competitionTournamentRoundsSet = new HashSet<>();
+        competitionTournamentRoundsSet.add(competitionTournamentRounds);
+        teams.forEach(t -> t.setCompetitions(competitionTournamentRoundsSet));
+        when(teamService.findAllByCompetitions(competitionTournamentRounds)).thenReturn(teams);
+
+        Game gameForSave = Game.builder().id(1L).build();
+        Set<Game> gamesForSave = new HashSet<>();
+        gamesForSave.add(gameForSave);
+        when(gameService.save(any())).thenReturn(gameForSave);
+        when(gameService.saveAll(anySet())).thenReturn(gamesForSave);
+
+        Odd oddForSave = Odd.builder().id(1L).build();
+        when(oddService.save(any())).thenReturn(oddForSave);
+
+        ResultFootball resultFootballForSaved = ResultFootball.builder().id(1L).build();
+        Set<ResultFootball> resultsFootballForSaved = new HashSet<>();
+        resultsFootballForSaved.add(resultFootballForSaved);
+        when(resultFootballService.saveAll(anySet())).thenReturn(resultsFootballForSaved);
+
+        GameReportFootball gameReportFootballForSaved = GameReportFootball.builder().id(1L).build();
+        when(gameReportFootballService.save(any())).thenReturn(gameReportFootballForSaved);
+
+        doReturn(Team.builder().competitions(competitionTournamentRoundsSet).name("Chelsea").build()).when(teamService).findByName(anyString());
+
+        Set<Game> gamesWithoutOdds = fakeFootballService.generateGames(competitionTournamentRounds);
+        Set<Game> gamesWithOdds = fakeFootballService.generateOdds(gamesWithoutOdds);
+        Set<ResultFootball> result = fakeFootballService.generateResults(gamesWithOdds);
+
+        assertAll(() -> {
+            assertThat(gamesWithOdds, hasSize(1));
+            assertThat(result, hasSize(gamesWithOdds.size()));
+            assertThat(result.iterator().next().getGameReport().getGoalHome(), greaterThan(-1));
+            assertThat(result.iterator().next().getGameReport().getCornerAway(), greaterThan(-1));
+        });
+
+        verify(resultFootballService, times(1)).saveAll(anySet());
+        verify(competitionService, times(teams.size())).save(any());
+        verify(teamService, times(teams.size())).save(any());
+        verify(teamService, times(teams.size())).findByName(anyString());
     }
 
     @Test
@@ -163,12 +229,10 @@ class FakeFootballServiceTest {
         odds.add(oddX);
 
         competitions.add(competition);
+
         Game game = Game.builder().id(1L)
-                                  .teamHome(Team.builder()
-                                          .name("Chelsea")
-                                          .competitions(competitions).build())
-                                  .teamAway(Team.builder().name("Arsenal")
-                                          .competitions(competitions).build())
+                                  .teamHome(chelseaTeam)
+                                  .teamAway(arsenalTeam)
                                   .odds(odds)
                                   .build();
 
