18cb8c594d5f9debd74991bfd291a763dee1312c
Małgorzata Dubel <goska.dubel@gmail.com>
2020-06-01 23:02:58 +0200

    #94 Implement swagger 2

--- src/main/java/com/pulawskk/sportseventapi/AppConfig.java
+++ src/main/java/com/pulawskk/sportseventapi/AppConfig.java
@@ -10,12 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Configuration
 @EnableScheduling
+@EnableSwagger2
 public class AppConfig {
 
     @Autowired
@@ -40,5 +46,12 @@ public class AppConfig {
         return new HttpPost();
     }
 
-
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
